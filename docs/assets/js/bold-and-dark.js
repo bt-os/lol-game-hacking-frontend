82157dc5a46855308e15d80f74c6e05dcd2ba7d0
Vic P <vic4key@gmail.com>
2022-11-27 04:04:26 +0700

    Initial

--- docs/assets/js/bold-and-dark.js
+++ docs/assets/js/bold-and-dark.js
@@ -1,100 +1,109 @@
 (function() {
-  "use strict"; // Start of use strict
+	"use strict"; // Start of use strict
 
-  function http_get_json_object(url)
-  {
-    return $.ajax({
-      url: url,
-      type: "GET",
-      dataType: "json",
-      cache: false,
-      async: false
-    }).responseJSON;
-  }
+	function http_get_json_object(url)
+	{
+		return $.ajax({
+			url: url,
+			type: "GET",
+			dataType: "json",
+			cache: false,
+			async: false
+		}).responseJSON;
+	}
 
-  window.addEventListener("DOMContentLoaded", (event) => {
-    // let navbar_toggler = document.querySelector(".navbar-toggler");
-    // if (navbar_toggler) navbar_toggler.click();
+	window.addEventListener("DOMContentLoaded", (event) =>
+	{
+		// let navbar_toggler = document.querySelector(".navbar-toggler");
+		// if (navbar_toggler) navbar_toggler.click();
 
-    let strings = null; // http_get_json_object("https://raw.githubusercontent.com/bt-os/lol-game-hacking-frontend/main/data/strings.json")
-    if (strings)
-    {
-      $("#scrolling-text-label-text")?.first()?.html(strings.scrolling_text_label_text);
+		let timer_live_chat_ads = setInterval(() =>
+		{
+			let live_chat_ads = $("div a[href*='elfsight.com']")?.first()?.parent();
+			if (live_chat_ads?.is(":visible"))
+			{
+				live_chat_ads.hide();
+				clearInterval(timer_live_chat_ads);
+			}
+		}, 1000);
 
-      $("#slider-slide-0-label-text-1")?.first()?.html(strings.slider[0].label_text_1);
-      $("#slider-slide-0-label-text-2")?.first()?.html(strings.slider[0].label_text_2);
-      $("#slider-slide-0-label-text-3")?.first()?.html(strings.slider[0].label_text_3);
+		let strings = null; // http_get_json_object("https://raw.githubusercontent.com/bt-os/lol-game-hacking-frontend/main/data/strings.json")
+		if (strings)
+		{
+			$("#scrolling-text-label-text")?.first()?.html(strings.scrolling_text_label_text);
 
-      $("#streamer-1-label-text-1")?.first()?.html(strings.slider[1].label_text_1);
-      $("#streamer-1-label-text-2")?.first()?.html(strings.slider[1].label_text_2);
-      $("#streamer-1-label-text-3")?.first()?.html(strings.slider[1].label_text_3);
-      $("#streamer-1-link-facebook")?.first()?.attr("href", strings.slider[1].link_facebook);
-      $("#streamer-1-link-instagram")?.first()?.attr("href", strings.slider[1].link_instagram);
-      $("#streamer-1-link-youtube")?.first()?.attr("href", strings.slider[1].link_youtube);
+			$("#slider-slide-0-label-text-1")?.first()?.html(strings.slider[0].label_text_1);
+			$("#slider-slide-0-label-text-2")?.first()?.html(strings.slider[0].label_text_2);
+			$("#slider-slide-0-label-text-3")?.first()?.html(strings.slider[0].label_text_3);
 
-      $("#streamer-2-label-text-1")?.first()?.html(strings.slider[2].label_text_1);
-      $("#streamer-2-label-text-2")?.first()?.html(strings.slider[2].label_text_2);
-      $("#streamer-2-label-text-3")?.first()?.html(strings.slider[2].label_text_3);
-      $("#streamer-2-link-facebook")?.first()?.attr("href", strings.slider[1].link_facebook);
-      $("#streamer-2-link-instagram")?.first()?.attr("href", strings.slider[1].link_instagram);
-      $("#streamer-2-link-youtube")?.first()?.attr("href", strings.slider[1].link_youtube);
-    }
-  });
+			$("#streamer-1-label-text-1")?.first()?.html(strings.slider[1].label_text_1);
+			$("#streamer-1-label-text-2")?.first()?.html(strings.slider[1].label_text_2);
+			$("#streamer-1-label-text-3")?.first()?.html(strings.slider[1].label_text_3);
+			$("#streamer-1-link-facebook")?.first()?.attr("href", strings.slider[1].link_facebook);
+			$("#streamer-1-link-instagram")?.first()?.attr("href", strings.slider[1].link_instagram);
+			$("#streamer-1-link-youtube")?.first()?.attr("href", strings.slider[1].link_youtube);
 
-  function initParallax() {
+			$("#streamer-2-label-text-1")?.first()?.html(strings.slider[2].label_text_1);
+			$("#streamer-2-label-text-2")?.first()?.html(strings.slider[2].label_text_2);
+			$("#streamer-2-label-text-3")?.first()?.html(strings.slider[2].label_text_3);
+			$("#streamer-2-link-facebook")?.first()?.attr("href", strings.slider[1].link_facebook);
+			$("#streamer-2-link-instagram")?.first()?.attr("href", strings.slider[1].link_instagram);
+			$("#streamer-2-link-youtube")?.first()?.attr("href", strings.slider[1].link_youtube);
+		}
+	});
 
-    if (!('requestAnimationFrame' in window)) return;
-    if (/Mobile|Android/.test(navigator.userAgent)) return;
+	function initParallax()
+	{
+		if (!('requestAnimationFrame' in window)) return;
+		if (/Mobile|Android/.test(navigator.userAgent)) return;
 
-    var parallaxItems = document.querySelectorAll('[data-bss-parallax]');
+		var parallaxItems = document.querySelectorAll('[data-bss-parallax]');
+		if (!parallaxItems.length) return;
 
-    if (!parallaxItems.length) return;
+		var defaultSpeed = 0.5;
+		var visible = [];
+		var scheduled;
 
-    var defaultSpeed = 0.5;
-    var visible = [];
-    var scheduled;
+		window.addEventListener('scroll', scroll);
+		window.addEventListener('resize', scroll);
 
-    window.addEventListener('scroll', scroll);
-    window.addEventListener('resize', scroll);
+		scroll();
 
-    scroll();
+		function scroll()
+		{
+			visible.length = 0;
 
-    function scroll() {
+			for (var i = 0; i < parallaxItems.length; i++) {
+				var rect = parallaxItems[i].getBoundingClientRect();
+				var speed = parseFloat(parallaxItems[i].getAttribute('data-bss-parallax-speed'), 10) || defaultSpeed;
 
-      visible.length = 0;
+				if (rect.bottom > 0 && rect.top < window.innerHeight) {
+					visible.push({
+						speed: speed,
+						node: parallaxItems[i]
+					});
+				}
 
-      for (var i = 0; i < parallaxItems.length; i++) {
-        var rect = parallaxItems[i].getBoundingClientRect();
-        var speed = parseFloat(parallaxItems[i].getAttribute('data-bss-parallax-speed'), 10) || defaultSpeed;
+			}
 
-        if (rect.bottom > 0 && rect.top < window.innerHeight) {
-          visible.push({
-            speed: speed,
-            node: parallaxItems[i]
-          });
-        }
+			cancelAnimationFrame(scheduled);
 
-      }
+			if (visible.length)
+			{
+				scheduled = requestAnimationFrame(update);
+			}
+		}
 
-      cancelAnimationFrame(scheduled);
+		function update()
+		{
+			for (var i = 0; i < visible.length; i++)
+			{
+				var node = visible[i].node;
+				var speed = visible[i].speed;
+				node.style.transform = 'translate3d(0, ' + (-window.scrollY * speed) + 'px, 0)';
+			}
+		}
+	}
 
-      if (visible.length) {
-        scheduled = requestAnimationFrame(update);
-      }
-
-    }
-
-    function update() {
-
-      for (var i = 0; i < visible.length; i++) {
-        var node = visible[i].node;
-        var speed = visible[i].speed;
-
-        node.style.transform = 'translate3d(0, ' + (-window.scrollY * speed) + 'px, 0)';
-      }
-
-    }
-  }
-
-  initParallax();
+	initParallax();
 })(); // End of use strict
